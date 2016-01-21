e596f3da5b5779466bced5d14958c9d1646f1f7a
ThomasO <topsomer@theassets.co>
2016-01-21 16:58:33 +0100

    Add luigi config files

---
+++ src/luigi/luigid.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# @Author: ThomasO
+# @Date:   2016-01-21 11:36:16
+# @Last Modified by:   ThomasO
+# @Last Modified time: 2016-01-21 16:56:02
+
+
+exec /usr/local/bin/luigid >>/var/log/luigid/luigid.log 2>&1
